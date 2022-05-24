f44a705cf8f81ee1dd5bf1901cfb9499150e8fdb
PraneelChugh <86180715+PraneelChugh@users.noreply.github.com>
2022-05-24 19:17:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 song = "";
+leftWristY = 0;
+leftWristX = 0;
+
+rightWristY = 0;
+rightWristX = 0;
 
 function preload(){
     song = loadSound("music.mp3");
@@ -11,10 +16,30 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristx = " + leftWristX + "leftWristY = " + leftWristY );
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY= results[0].pose.rightWrist.y;
+        console.log("rightWristx = " + rightWristX + "rightWristY = " + rightWristY );
+    }
+}
+
 function draw(){
     image(video,0,0,600,500);
 }
+
 function play(){
     song.play();
 }
