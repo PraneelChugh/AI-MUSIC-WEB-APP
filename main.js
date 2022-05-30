eacba6ec338772cb4bb9f8d5bffc1d21f7400fe7
PraneelChugh <86180715+PraneelChugh@users.noreply.github.com>
2022-05-30 08:47:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-song = "";
+song= ""
 leftWristY = 0;
 leftWristX = 0;
 
 rightWristY = 0;
 rightWristX = 0;
 
+scoreLeftWrist = 0;
+
+leftWrist = 0;
+rightWrist  = 0;
+
 function preload(){
-    song = loadSound("music.mp3");
-    song = loadSound("music2.mp3")
+   song = loadSound("music.mp3", "music2.mp3");
+   if(rightWrist == "music.mp3", leftWrist == "music2.mp3"){
+
+   }
 }
 
 function setup(){
@@ -26,18 +33,28 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristx = " + leftWristX + "leftWristY = " + leftWristY );
 
         rightWristX = results[0].pose.rightWrist.x;
-        rightWristY= results[0].pose.rightWrist.y;
+        ;rightWristY= results[0].pose.rightWrist.y;
         console.log("rightWristx = " + rightWristX + "rightWristY = " + rightWristY );
     }
 }
 
+function modelLoaded(){
+    console.log('PoseNet is Initialised');
+}
+
 function draw(){
     image(video,0,0,600,500);
+    fill('#FF0000');
+    stroke('#FF0000');
+    circle(leftWristX,leftWristY,20);
 }
 
 function play(){
